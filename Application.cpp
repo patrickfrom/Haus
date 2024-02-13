4e9f85219699974531f6c10bad65dccdf9c240e2
PatrickFrom <patrickfromgnomedk@gmail.com>
2024-02-13 13:22:57 +0000

    Created Swapchain

--- Application.cpp
+++ Application.cpp
@@ -60,8 +60,10 @@ namespace Haus {
     void Application::InitVulkan() {
         std::cout << "Initializing Vulkan" << std::endl;
         CreateInstance();
+        CreateSurface();
         PickPhysicalDevice();
         CreateLogicalDevice();
+        CreateSwapchain();
     }
 
     void Application::CreateInstance() {
@@ -72,11 +74,11 @@ namespace Haus {
                 .pEngineName = "No Engine",
                 .engineVersion = VK_MAKE_VERSION(1, 0, 0),
 
-                .apiVersion = VK_API_VERSION_1_0
+                .apiVersion = VK_API_VERSION_1_2
         };
 
         uint32_t glfwExtensionCount = 0;
-        const char** extensions = glfwGetRequiredInstanceExtensions(&glfwExtensionCount);
+        const char **extensions = glfwGetRequiredInstanceExtensions(&glfwExtensionCount);
 
         vk::InstanceCreateInfo createInfo{
                 .pApplicationInfo = &appInfo,
@@ -87,6 +89,11 @@ namespace Haus {
         m_Instance = vk::createInstance(createInfo);
         if (!m_Instance)
             throw std::runtime_error("Failed to create Instance");
+    }
+
+    void Application::CreateSurface() {
+        if (glfwCreateWindowSurface(m_Instance, m_Window, nullptr, reinterpret_cast<VkSurfaceKHR *>(&m_Surface)))
+            throw std::runtime_error("Failed to create window surface!");
     }
 
     bool IsDeviceSuitable(vk::PhysicalDevice device) {
@@ -101,7 +108,7 @@ namespace Haus {
         if (devices.empty())
             throw std::runtime_error("Failed to find GPUs with vulkan support");
 
-        for (const auto& device : devices) {
+        for (const auto &device: devices) {
             if (IsDeviceSuitable(device)) {
                 m_PhysicalDevice = device;
                 break;
@@ -113,10 +120,81 @@ namespace Haus {
     }
 
     void Application::CreateLogicalDevice() {
+        float priority = 1.0f;
+        vk::DeviceQueueCreateInfo queueCreateInfo{
+                .queueFamilyIndex = 0,
+                .queueCount = 1,
+                .pQueuePriorities = &priority,
+        };
 
+        vk::PhysicalDeviceFeatures deviceFeatures{};
+
+        std::array<const char *, 1> enabledExtensions = {"VK_KHR_swapchain"};
+
+        vk::DeviceCreateInfo createInfo{
+                .queueCreateInfoCount = 1,
+                .pQueueCreateInfos = &queueCreateInfo,
+
+                .enabledExtensionCount = static_cast<uint32_t>(enabledExtensions.size()),
+                .ppEnabledExtensionNames = enabledExtensions.data(),
+
+                .pEnabledFeatures = &deviceFeatures,
+        };
+
+        m_Device = m_PhysicalDevice.createDevice(createInfo);
+        if (!m_Device)
+            throw std::runtime_error("Failed to create logical device");
+
+        m_GraphicsQueue = m_Device.getQueue(0, 0);
+    }
+
+    SwapChainSupportDetails Application::QuerySwapChainSupport(vk::PhysicalDevice device) {
+        SwapChainSupportDetails details {
+            .Capabilities = device.getSurfaceCapabilitiesKHR(m_Surface),
+            .Formats = device.getSurfaceFormatsKHR(m_Surface),
+            .PresentModes = device.getSurfacePresentModesKHR(m_Surface)
+        };
+
+        return details;
+    }
+
+    void Application::CreateSwapchain() {
+        SwapChainSupportDetails swapChainSupport = QuerySwapChainSupport(m_PhysicalDevice);
+        int width, height;
+        glfwGetFramebufferSize(m_Window, &width, &height);
+        vk::Extent2D extent{
+                .width = static_cast<uint32_t>(width),
+                .height = static_cast<uint32_t>(height)
+        };
+
+        vk::SwapchainCreateInfoKHR createInfo{
+                .surface = m_Surface,
+                .minImageCount = 4,
+                .imageFormat = swapChainSupport.Formats[0].format,
+                .imageColorSpace = swapChainSupport.Formats[0].colorSpace,
+                .imageExtent = extent,
+                .imageArrayLayers = 1,
+                .imageUsage = vk::ImageUsageFlagBits::eColorAttachment,
+                .imageSharingMode = vk::SharingMode::eExclusive,
+                .preTransform = swapChainSupport.Capabilities.currentTransform,
+                .compositeAlpha = vk::CompositeAlphaFlagBitsKHR::eOpaque,
+                .presentMode = vk::PresentModeKHR::eFifo,
+                .clipped = VK_TRUE,
+        };
+
+        if (m_Device.createSwapchainKHR(&createInfo, nullptr, &m_Swapchain) != vk::Result::eSuccess)
+            throw std::runtime_error("Failed to create swap chain");
+
+        m_SwapchainImages = m_Device.getSwapchainImagesKHR(m_Swapchain);
+
+        m_SwapchainImageFormat = swapChainSupport.Formats[0].format;
+        m_SwapchainExtent = extent;
     }
 
     void Application::CleanupVulkan() {
+        m_Device.destroySwapchainKHR(m_Swapchain);
+        m_Device.destroy();
+        m_Instance.destroySurfaceKHR(m_Surface);
         m_Instance.destroy();
     }
 
--- Application.h
+++ Application.h
@@ -12,6 +12,12 @@ namespace Haus {
         int Height;
     };
 
+    struct SwapChainSupportDetails {
+        vk::SurfaceCapabilitiesKHR Capabilities;
+        std::vector<vk::SurfaceFormatKHR> Formats;
+        std::vector<vk::PresentModeKHR> PresentModes;
+    };
+
     class Application {
     public:
         explicit Application(ApplicationSpecification& specification);
@@ -28,16 +34,28 @@ namespace Haus {
 
         GLFWwindow* m_Window;
     private:
+        SwapChainSupportDetails QuerySwapChainSupport(vk::PhysicalDevice device);
+
         void InitVulkan();
         void CreateInstance();
+        void CreateSurface();
         void PickPhysicalDevice();
         void CreateLogicalDevice();
+        void CreateSwapchain();
 
         void CleanupVulkan();
 
         vk::Instance m_Instance;
         vk::PhysicalDevice m_PhysicalDevice;
         vk::Device m_Device;
+
+        vk::SurfaceKHR m_Surface;
+        vk::SwapchainKHR m_Swapchain;
+        std::vector<vk::Image> m_SwapchainImages;
+        vk::Format m_SwapchainImageFormat;
+        vk::Extent2D m_SwapchainExtent;
+
+        vk::Queue m_GraphicsQueue;
     };
 
 } // Haus
